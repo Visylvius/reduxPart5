a765dd0e4a494607365586806f28b4432eed0302
Loren Barrick <lorenbarrick@gmail.com>
2016-06-10 13:01:29 -0700

    working on routes for router, creating form for user to post blog

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,21 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts-index';
-
+import PostsNew from './components/posts-new';
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={PostsIndex} />
+    <Route path='posts/new' component={PostsNew} />
   </Route>
 );
+
+
+// try using onEnter instead of componentWillMount
+//import fetchPosts from ''
+//so you could create a store in reducers, then export the store
+//then call store.dispatch(fetchPosts)
+//have access to the store, or remember to bind fetchPosts
+//onEnter={() => fetchPosts()}
+
+//once you've done that turn it into a functional components
